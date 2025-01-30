17824255bc025ac870e2510a86fdce70d6376c60
Lee <300086267@fvtc.edu>
2025-01-30 13:43:35 -0600

    second commit

--- site.js
+++ site.js
@@ -1,4 +1,26 @@
 
+
+const titleInput = document.querySelector('#title')
+const authorInput = document.querySelector('#author')
+const pagesInput = document.querySelector('#pages')
+const pagesDisplay = document.querySelector('#pages-display')
+const addBookButton = document.querySelector('#add')
+
+addBookButton.addEventListener('click', () => { 
+    // alert(authorInput.value)
+    const newBook = {
+        title: titleInput.value,
+        author: authorInput.value,
+        pages: pagesInput.value
+    }
+    books.push(newBook)
+    generateBooklist()
+})
+
+pagesInput.addEventListener('input', () =>{
+    pagesInput.value = pagesDisplay.value
+})
+
 const bookTitles = [
     "The Catcher in the Rye",
     "1984",
@@ -10,7 +32,7 @@ const bookTitles = [
 const testBook1 = {
     title: "Fellowship of the Ring",
     author: "J.R.R. Tolkien",
-    pages: 1209
+    pages: 423
 }
 
 const title = "The Two Towers"
@@ -31,6 +53,7 @@ const books = [
         title: 'Dune',
         author: "Frank Herbert",
         pages: 412
+        // genres: ["123", "223"]
     },
     {
         title: "Dune Messiah",
@@ -41,8 +64,15 @@ const books = [
         title: "The Children of Dune",
         author: "Frank Herbert",
         pages: 444
+        
     }
 ]
+
+books.push({
+    title: "Harry Potter 1",
+    author: "JK Rowling",
+    pages: 233
+})
 
 const book01 = document.querySelector('#Top-books')
 // bookTitles.forEach(title => {
@@ -56,22 +86,46 @@ const book01 = document.querySelector('#Top-books')
     const { title, author } = books[0]
 }
 
-books.forEach(({title, author, pages}) => {
-    const li = document.createElement('li')
-    book01.append(li)
+function generateBooklist(){
 
-    const h2 = document.createElement('h2')
-    h2.textContent = title
-    li.append(h2)
+    book01.innerHTML = ''
 
-    const h3 = document.createElement('h3')
-    h3.textContent = author
-    li.append(h3)
+    books.forEach(({title, author, pages, genres}) => {
+        const li = document.createElement('li')
+        book01.append(li)
 
-    const p = document.createElement('p')
-    p.textContent = pages
-    li.append(p)
+        const h2 = document.createElement('h2')
+        h2.textContent = title
+        li.append(h2)
 
-})
+        const h3 = document.createElement('h3')
+        h3.textContent = author
+        li.append(h3)
 
+        const p = document.createElement('p')
+        p.textContent = pages
+        li.append(p)
 
+        if (!genres) return
+
+        const genresSpan = document.createElement('span')
+        genresSpan.textContent = 'Genres:'
+        li.append(genresSpan)
+
+        const genresUl = document.createElement('ul')
+        li.append(genresUl)
+
+        if(genres)
+        {
+
+            genres.forEach(genre => {
+                const genreLi = document.createElement('li')
+                genreLi.textContent = genre
+                genresUl.append(genreLi)
+            })
+
+        }
+
+    })
+
+}
